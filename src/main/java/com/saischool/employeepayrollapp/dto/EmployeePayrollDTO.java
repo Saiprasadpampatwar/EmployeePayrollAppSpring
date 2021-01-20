ef869914326bfdc4b53418ff9885cc281de13c69
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-20 12:37:07 +0530

    [Saiprasad]Added: ability to through user friendly response

--- src/main/java/com/saischool/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/saischool/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,11 +1,14 @@
 package com.saischool.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 
 public class EmployeePayrollDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Employee Name Invalid")
     public String name;
+
+    @Min(value = 500, message = "Min wage should be more than 500")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
