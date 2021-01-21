6d65ba03dff89dee4039c721fe343da56d67f4dd
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-21 10:17:39 +0530

    [Saiprasad]Added: remaining properties along with validation to dto and changed the model accordingly

--- src/main/java/com/saischool/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/saischool/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,9 +1,9 @@
 package com.saischool.employeepayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,10 +15,22 @@ public @ToString class EmployeePayrollDTO {
     @Min(value = 500, message = "Min wage should be more than 500")
     public long salary;
 
+    @Pattern(regexp = "^(male|female)$", message = "Invalid Employee Gender")
     public String gender;
+
+    @NotBlank(message = "ImagePath cannot be Empty")
     public String imagePath;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @NotNull(message = "start date should Not be Empty")
+    @PastOrPresent(message = "start date should be past or todays date")
     public LocalDate startDate;
+
+    @NotBlank(message = "Note cannot be Empty")
     public String notes;
+
+    @NotBlank(message = "Department cannot be Empty")
     public List<String> departments;
+
     public int employeeId;
 }
--- src/main/java/com/saischool/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/saischool/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -3,6 +3,7 @@ package com.saischool.employeepayrollapp.exceptions;
 import com.saischool.employeepayrollapp.dto.ResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +14,15 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class EmployeePayrollExceptionHandler {
+
+    private static final String message = "Exception while processing REST Request";
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException exception) {
+        ResponseDTO responseDTO = new ResponseDTO(message, "Should have date in format dd MM yyyy");
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
